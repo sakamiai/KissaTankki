85c4311899a0f0754dae42c5b28e7393acb4258d
sakamiai <sakamiai@student.jyu.fi>
2014-01-25 12:06:13 +0200

    muutoksia ja control panel

---
+++ Assets/scripts/forceMovement.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class forceMovement : MonoBehaviour {
+
+	public Transform left;
+	public Transform right;
+	public Transform front;
+
+	public float speed = 30f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 dir = (front.position - transform.position).normalized;
+
+		if (Input.GetKeyDown(KeyCode.LeftArrow)) {
+			rigidbody.AddForceAtPosition(dir * speed, left.position);
+		}
+
+		if (Input.GetKeyDown(KeyCode.RightArrow)) {
+			rigidbody.AddForceAtPosition(dir * speed, right.position);
+		}
+	}
+}
