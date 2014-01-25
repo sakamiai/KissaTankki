68c421aa191496a6f82ed1e4341db67f3d445320
sakamiai <sakamiai@student.jyu.fi>
2014-01-25 17:22:22 +0200

    controlleja

---
+++ Assets/scripts/ThrottleVipuRotate.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ThrottleVipuRotate : MonoBehaviour {
+
+	public float CoefOfRotation = 1f;
+
+	// Use this for initialization
+	void Start () {
+
+		Throttle.OnValueAdded += HandleOnValueChangedPL;
+		Throttle.OnValueDecreased += HandleOnValueChangedMI;	
+	}
+	
+	void OnDisable() {
+		Throttle.OnValueAdded -= HandleOnValueChangedPL;
+		Throttle.OnValueDecreased -= HandleOnValueChangedMI;	
+	}
+
+	void HandleOnValueChangedPL ()
+	{
+		transform.Rotate(new Vector3(0,-CoefOfRotation,0)) ;
+	}
+
+	void HandleOnValueChangedMI ()
+	{
+		transform.Rotate(new Vector3(0,CoefOfRotation,0)) ;
+	}
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
