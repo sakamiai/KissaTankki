14594db9d0c5b88d737b79f4ed096704a8460b86
sakamiai <sakamiai@student.jyu.fi>
2014-01-26 02:59:22 +0200

    paljon muutoksia

--- Assets/scripts/TowerTurn.cs
+++ Assets/scripts/TowerTurn.cs
@@ -5,13 +5,22 @@ public class TowerTurn : MonoBehaviour {
 
 	public float turnSpeed = 0.7f;
 
-	public float vertical = 0f;
-	public float horizontal = 0f;
 
-	public float maxHorizontal = 45f;
-	public float minHorizontal = -20f;
+	//public float vertical = 0f;
+	//public float horizontal = 0f;
 
-	public TankHealth condition;
+	public float rotation;
+
+	public float maxRotation = 45f;
+	public float minRotation = -20f;
+	public bool limits = false;
+
+	public KeyCode positive;
+	public KeyCode negative;
+
+	public int horizontal = 0;
+	public int vertical = 0;
+
 
 	// Use this for initialization
 	void Start () {
@@ -20,27 +29,37 @@ public class TowerTurn : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-		if (Input.GetKey(KeyCode.PageUp)) {
-			//transform.RotateAround(transform.position, transform.up, turnSpeed);
-			vertical += turnSpeed;
-		}
-		if (Input.GetKey(KeyCode.PageDown)) {
+		//if (Input.GetKey(left)) {
+			//transform.Rotate(transform.position, transform.up, turnSpeed);
+		//	vertical += turnSpeed;
+		//}
+		//if (Input.GetKey(right)) {
 			//transform.RotateAround(transform.position, transform.up, -turnSpeed);
-			vertical -= turnSpeed;
-		}
+		//	vertical -= turnSpeed;
+		//}
 
-		if (Input.GetKey(KeyCode.Home) && horizontal < maxHorizontal) {
+		if (Input.GetKey(positive) && (rotation < maxRotation || !limits)) {
 			//transform.RotateAround(transform.position, transform.forward, turnSpeed);
-			horizontal += turnSpeed;
+			rotation += turnSpeed;
 		}
-		if (Input.GetKey(KeyCode.End) && horizontal > minHorizontal) {
+		if (Input.GetKey(negative) && (rotation > minRotation || !limits)) {
 			//transform.RotateAround(transform.position, transform.forward, -turnSpeed);
-			horizontal -= turnSpeed;
+			rotation -= turnSpeed;
 		}
 
-		transform.rotation = transform.parent.rotation;
-		transform.RotateAround(transform.position, Vector3.forward, horizontal);
-		transform.RotateAround(transform.position, Vector3.up, vertical);
+		if(limits){
+			if((transform.localRotation * Quaternion.Euler(new Vector3(0, 0, vertical * rotation))).eulerAngles.z > maxRotation)
+			transform.localRotation = transform.localRotation * Quaternion.Euler(new Vector3(0, 0, vertical * rotation));
+			rotation = 0 ;
+		}
+		else {
+			transform.rotation = transform.parent.rotation * Quaternion.Euler(new Vector3(0, horizontal * rotation, 0));
+		}
+		//		transform.Rotate(Vector3.forward, horizontal);
+//		transform.Rotate(Vector3.up, vertical);
+
+		//transform.RotateAround(transform.position, Vector3.forward, horizontal);
+		//transform.RotateAround(transform.position, Vector3.up, vertical);
 
 	}
 }
