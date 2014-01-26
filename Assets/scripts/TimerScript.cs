768c6993d1e953f78fe2857ca7f877ac231275e1
Maqqr <maqqr@server.fake>
2014-01-26 14:56:10 +0200

    timer

---
+++ Assets/scripts/TimerScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TimerScript : MonoBehaviour {
+
+	float timer = 0f;
+	float endTime = 10f;
+
+	int nextLevelNumber = 0;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timer += Time.deltaTime;
+
+		if (timer > endTime) {
+			Application.LoadLevel(nextLevelNumber);
+		}
+	}
+}
